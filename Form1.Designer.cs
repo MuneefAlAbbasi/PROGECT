df751dd426faf7570bd9f0cdd52f08d1419bee35
MuneefAlAbbasi <mohs.s28js2.m@gmail.com>
2025-02-23 22:03:15 +0300

    Add files via upload
    c sharp

---
+++ Form1.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace progectt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.user = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.password = new System.Windows.Forms.TextBox();
+            this.date = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.male = new System.Windows.Forms.RadioButton();
+            this.famle = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.contery = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(106, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User name";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(24, 263);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "email";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.contery);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.date);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.password);
+            this.groupBox1.Controls.Add(this.email);
+            this.groupBox1.Controls.Add(this.user);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(810, 642);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // user
+            // 
+            this.user.Location = new System.Drawing.Point(192, 50);
+            this.user.Name = "user";
+            this.user.Size = new System.Drawing.Size(176, 24);
+            this.user.TabIndex = 3;
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(192, 267);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(176, 24);
+            this.email.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(192, 557);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(419, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.result.Location = new System.Drawing.Point(845, 46);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(72, 29);
+            this.result.TabIndex = 4;
+            this.result.Text = "result";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(15, 476);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 24);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "password";
+            this.label5.Click += new System.EventHandler(this.Label2_Click);
+            // 
+            // password
+            // 
+            this.password.Location = new System.Drawing.Point(183, 480);
+            this.password.Name = "password";
+            this.password.PasswordChar = '*';
+            this.password.Size = new System.Drawing.Size(176, 24);
+            this.password.TabIndex = 5;
+            // 
+            // date
+            // 
+            this.date.Location = new System.Drawing.Point(555, 101);
+            this.date.Name = "date";
+            this.date.Size = new System.Drawing.Size(200, 24);
+            this.date.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(551, 50);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "select date";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.famle);
+            this.groupBox2.Controls.Add(this.male);
+            this.groupBox2.Location = new System.Drawing.Point(555, 220);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Enter += new System.EventHandler(this.GroupBox2_Enter);
+            // 
+            // male
+            // 
+            this.male.AutoSize = true;
+            this.male.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.male.Location = new System.Drawing.Point(13, 31);
+            this.male.Name = "male";
+            this.male.Size = new System.Drawing.Size(73, 28);
+            this.male.TabIndex = 0;
+            this.male.TabStop = true;
+            this.male.Text = "Male";
+            this.male.UseVisualStyleBackColor = true;
+            // 
+            // famle
+            // 
+            this.famle.AutoSize = true;
+            this.famle.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.famle.Location = new System.Drawing.Point(117, 31);
+            this.famle.Name = "famle";
+            this.famle.Size = new System.Drawing.Size(85, 28);
+            this.famle.TabIndex = 1;
+            this.famle.TabStop = true;
+            this.famle.Text = "Famle";
+            this.famle.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(551, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(49, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "type";
+            this.label6.Click += new System.EventHandler(this.Label6_Click);
+            // 
+            // contery
+            // 
+            this.contery.FormattingEnabled = true;
+            this.contery.Items.AddRange(new object[] {
+            "Yemen",
+            "qter",
+            "sadi"});
+            this.contery.Location = new System.Drawing.Point(555, 480);
+            this.contery.Name = "contery";
+            this.contery.Size = new System.Drawing.Size(187, 24);
+            this.contery.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(551, 413);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(134, 24);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "select contery";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1094, 666);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton famle;
+        private System.Windows.Forms.RadioButton male;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker date;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox password;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.TextBox user;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox contery;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace progectt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GroupBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if(string.IsNullOrEmpty(user.Text)||string.IsNullOrEmpty(password.Text)|| string.IsNullOrEmpty(contery.Text) || string.IsNullOrEmpty(date.Text) || string.IsNullOrEmpty(email.Text)){
+                MessageBox.Show("plase full the fields");
+
+            }
+          else  if(!male.Checked&&!famle.Checked)
+                {
+                    MessageBox.Show("plase select the type");
+                    return;
+
+                }
+            else
+            {
+             if(!IsValidEmail(email.Text))
+                {
+                    MessageBox.Show("plase enter email from correct");
+                    return;
+
+                }
+             
+           
+                String type = male.Checked ? "male" : "famle";
+                result.Text = $"user name is {user.Text}\n email is {email.Text}\n paswword is {password.Text}\n date is {date.Text} \n countery is {contery.Text}\n type is {type}";
+
+            }
+        }
+     
+        public static bool IsValidEmail(string email)
+        {
+            // تعبير عادي للتحقق من صحة البريد الإلكتروني
+            string pattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
+            return Regex.IsMatch(email, pattern);
+        }
+
+    }
+}
